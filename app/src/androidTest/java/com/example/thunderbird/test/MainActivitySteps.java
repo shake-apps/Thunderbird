257042db5db24bc6b230dd13589c4b2a2ace44e3
Nick Robinson <grobins2@gmail.com>
2015-11-18 16:13:56 -0500

    first feature test

--- app/src/androidTest/java/com/example/thunderbird/test/MainActivitySteps.java
+++ app/src/androidTest/java/com/example/thunderbird/test/MainActivitySteps.java
@@ -4,31 +4,31 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.example.thunderbird.MainActivity;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-/**
- * Created by nrobin01 on 11/18/15.
- */
+@CucumberOptions(features = "features")
 public class MainActivitySteps extends ActivityInstrumentationTestCase2
 	<MainActivity> {
 
 		public MainActivitySteps(){
 			super(MainActivity.class);
-			assertNotNull(getActivity());
 		}
 
 		@Given("^I am on the main activity$")
-		public void I_have_my_app_configured() {
+		public void I_am_on_the_main_activity() {
+			assertEquals(1, 1);
+			assertNotNull(getActivity());
 		}
 
-		@When("^I tap the continue button$")
-		public void something_happens(final char op) {
-
-		}
-
-		@Then("^I should see the congratulations view$")
-		public void I_should_see_xx_on_the_display(final String s) {
-		}
+//		@When("^I tap the continue button$")
+//		public void something_happens(final char op) {
+//
+//		}
+//
+//		@Then("^I should see the congratulations view$")
+//		public void I_should_see_xx_on_the_display(final String s) {
+//		}
 }
